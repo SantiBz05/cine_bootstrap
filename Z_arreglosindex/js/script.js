05d3ca1ff524e8870587414629d8fc371eece4ee
Santi Baez <s.baez@itecriocuarto.org.ar>
2024-08-14 19:20:04 -0300

    Arreglos Index

--- Z_arreglosindex/js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('show-gallery1').addEventListener('click', function() {
-  document.getElementById('gallery1').classList.remove('d-none');
-  document.getElementById('gallery2').classList.add('d-none');
-});
-
-document.getElementById('show-gallery2').addEventListener('click', function() {
-  document.getElementById('gallery1').classList.add('d-none');
-  document.getElementById('gallery2').classList.remove('d-none');
-});
